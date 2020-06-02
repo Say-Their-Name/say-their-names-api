70480609d8dce6cdb108a7bfcd05e4256f127465
xtremebeing <tcarter@entrusion.com>
2020-06-02 19:22:24 -0400

    Added SearchPeople endpoint and Controller (#14)
    * Added SearchPeople endpoint and Controller
    
    * Added SearchPeople endpoint and Controller

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\API\Donations\GetSingleDonationController;
 use App\Http\Controllers\API\Donations\ListDonationsController;
 use App\Http\Controllers\API\People\GetSinglePersonController;
 use App\Http\Controllers\API\People\ListPeopleController;
+use App\Http\Controllers\API\People\SearchPeopleController;
 use App\Http\Controllers\API\Petitions\GetSinglePetitionController;
 use App\Http\Controllers\API\Petitions\ListPetitionsController;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,7 @@ Route::middleware('api')->group(function () {
 
     Route::get('people', ListPeopleController::class);
     Route::get('people/{person}', GetSinglePersonController::class);
+    Route::get('people/search/{person}', SearchPeopleController::class);
 
     Route::get('donations', ListDonationsController::class);
     Route::get('donations/{donation}', GetSingleDonationController::class);
