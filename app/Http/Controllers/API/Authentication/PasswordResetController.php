2c51b0174fba941e31a223691e409ba8c93b99d6
devingray <development@devingray.tech>
2020-06-02 22:32:41 +0700

    Password Resets

--- app/Http/Controllers/API/Authentication/PasswordResetController.php
+++ app/Http/Controllers/API/Authentication/PasswordResetController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\API\Authentication;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ResetPasswordRequest;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class PasswordResetController extends Controller
 {
-    public function __invoke(ResetPasswordRequest $request)
+    use ResetsPasswords;
+
+    protected function rules()
     {
-        return response()->json('Success');
+        return [
+            'token' => 'required',
+            'email' => [
+                'required',
+                'email',
+                'exists:users'
+            ],
+            'password' => [
+                'required',
+                'confirmed',
+                'min:6'
+            ],
+        ];
     }
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+        return ['status' => trans($response)];
+    }
+
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return response()->json(['email' => trans($response)], 400);
+    }
+
 }
--- app/Http/Controllers/API/Authentication/RequestPasswordResetController.php
+++ app/Http/Controllers/API/Authentication/RequestPasswordResetController.php
@@ -3,13 +3,31 @@
 namespace App\Http\Controllers\API\Authentication;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\RequestPasswordResetRequest;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class RequestPasswordResetController extends Controller
 {
-    public function __invoke(RequestPasswordResetRequest $request)
-    {
-        return response()->json('Reset Email Has Been Sent');
+    use SendsPasswordResetEmails;
 
+    protected function validateEmail(Request $request)
+    {
+        $request->validate([
+            'email' => [
+                'required',
+                'email',
+                'exists:users'
+            ],
+        ]);
+    }
+
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return ['status' => trans($response)];
+    }
+
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return response()->json(['email' => trans($response)], 400);
     }
 }
