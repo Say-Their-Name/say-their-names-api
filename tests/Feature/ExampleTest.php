72dd47cdc3a3271eb312fce4cb3623c25916ddfa
jheard <jheard@pandora.com>
2020-06-02 16:49:23 -0400

    - Adding initial feature test for People API. - Await feedback before continuing to add more feature tests.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -17,4 +17,16 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * Test 404 for bogus URI.
+     *
+     * @return void
+     */
+    public function testBogusURI()
+    {
+        $response = $this->get('/fod030u4j3nofw');
+
+        $response->assertNotFound();
+    }
 }
