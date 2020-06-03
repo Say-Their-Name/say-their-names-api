206c8080e34a6c98ea4731b946433118f4cafb14
devingray <development@devingray.tech>
2020-06-03 09:46:38 +0700

    Base set up for tests

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Traits\Unguarded;
 use App\Notifications\ForgotPassword;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,10 +11,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
-
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    use Unguarded;
 
     protected $hidden = [
         'password', 'remember_token',
