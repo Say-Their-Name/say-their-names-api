071476d0583709254f4d41b4026ee89f3cd63798
Marina Gornostaeva <gornostaevamv@gmail.com>
2021-10-20 21:43:10 +0200

    (try) fix inconsistent person_id in json

--- app/Models/PersonImages.php
+++ app/Models/PersonImages.php
@@ -6,5 +6,11 @@ use App\Models\Traits\BelongsToPerson;
 
 class PersonImages extends BaseModel
 {
+    // Note: without this, person_id ends up being a string in JSON and breaks the clients. 
+    // Couldn't figure out why it's not an integer like other foreign keys.
+    protected $casts = [
+        'person_id' => 'integer',
+    ];
+
     use BelongsToPerson;
 }
